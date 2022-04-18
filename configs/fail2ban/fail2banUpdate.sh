fea8625b43f860d1905f8e2ba94c9bacf4b8a619
Lucas van Staden <sales@proxiblue.com.au>
2022-04-18 08:02:15 +0800

    add in cron update of fail2ban lists

---
+++ configs/fail2ban/fail2banUpdate.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl https://raw.githubusercontent.com/ProxiBlue/HostingEnvironment/main/configs/fail2ban/filter.d/nginx-badurls.conf > /etc/fail2ban/filter.d/nginx-badurls.conf
+service fail2ban restart
+
